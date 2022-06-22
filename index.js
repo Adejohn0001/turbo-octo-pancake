4d4ecf37db22269d31798fe809f27ad559317cfb
Adejohn0001 <104025025+Adejohn0001@users.noreply.github.com>
2022-06-22 16:29:02 +0100

    Add files via upload
    uploaded my files

---
+++ index.js
@@ -0,0 +1,10 @@
+var firstName = "Oluwapelumi";
+var lastName = "Ojo";
+var height = 4.5;
+var country = "Nigeria";
+
+let allDisplay = "My name is " + firstName + " " + lastName + ". I am " + height + "ft tall" + ", and I am from " + country + ".";
+
+document.getElementById("display").innerHTML = allDisplay;
+
+
